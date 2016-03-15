5c8dcddc9078f0fb5947d7e9492eb980d7bbbd4e
jrt54 <jrt5493@gmail.com>
2016-03-15 05:37:13 -0500

    algo updates

---
+++ linux-gnu-c-debug/obj/Users/Idekia/Documents/CAAM520/CAAM520SRM/src/srmg/srmg.d
@@ -0,0 +1,77 @@
+/Users/Idekia/Documents/CAAM520/CAAM520SRM/linux-gnu-c-debug/obj//Users/Idekia/Documents/CAAM520/CAAM520SRM/src/srmg/srmg.o: \
+  /Users/Idekia/Documents/CAAM520/CAAM520SRM/src/srmg/srmg.c \
+  /Users/Idekia/Documents/petsc/petsc/include/petsc/private/pcimpl.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscksp.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscpc.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscmat.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscvec.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscis.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscsys.h \
+  /Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/petscconf.h \
+  /Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/petscfix.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscversion.h \
+  /Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/mpi.h \
+  /Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/mpio.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscmath.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscerror.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscviewertypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscoptions.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petsclog.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscsftypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscistypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscviewer.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscdrawtypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscpctypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petscdmtypes.h \
+  /Users/Idekia/Documents/petsc/petsc/include/petsc/private/petscimpl.h \
+  /Users/Idekia/Documents/CAAM520/CAAM520SRM/include/petscpcsrmg.h
+
+/Users/Idekia/Documents/petsc/petsc/include/petsc/private/pcimpl.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscksp.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscpc.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscmat.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscvec.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscis.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscsys.h:
+
+/Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/petscconf.h:
+
+/Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/petscfix.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscversion.h:
+
+/Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/mpi.h:
+
+/Users/Idekia/Documents/petsc/petsc/linux-gnu-c-debug/include/mpio.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscmath.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscerror.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscviewertypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscoptions.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petsclog.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscsftypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscistypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscviewer.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscdrawtypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscpctypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petscdmtypes.h:
+
+/Users/Idekia/Documents/petsc/petsc/include/petsc/private/petscimpl.h:
+
+/Users/Idekia/Documents/CAAM520/CAAM520SRM/include/petscpcsrmg.h:
