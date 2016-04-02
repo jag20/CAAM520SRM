484af722e427961008e232beabcf7213e79e9bd7
Matthew G. Knepley <knepley@gmail.com>
2016-04-02 16:04:42 -0500

    Scripts: Add code to calculate the shared library extension automatically

--- bin/patchautomation_ry.py
+++ bin/patchautomation_ry.py
@@ -1,17 +1,45 @@
 #! /usr/bin/env python
+import os, sys
+sys.path.insert(0, os.path.join(os.environ['PETSC_DIR'], 'config'))
+sys.path.insert(0, os.path.join(os.environ['PETSC_DIR'], 'config', 'BuildSystem'))
+
 import subprocess
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 
+import script
+class ConfigReader(script.Script):
+  def __init__(self):
+    import RDict
+    import os
 
+    argDB = RDict.RDict(None, None, 0, 0)
+    argDB.saveFilename = os.path.join(os.environ['PETSC_DIR'], os.environ['PETSC_ARCH'], 'lib','petsc','conf', 'RDict.db')
+    argDB.load()
+    script.Script.__init__(self, argDB = argDB)
+    return
+
+  def get(self, key):
+    return self.framework.argDB[key]
+
+  def getModule(self, modname):
+    return self.framework.require(modname, None)
+
+  def run(self):
+    self.setup()
+    self.framework = self.loadConfigure()
+
+config = ConfigReader()
+config.run()
+sharedExt = config.getModule('config.setCompilers').sharedLibraryExt
 Ns = []
 errors = []
 
 k = 6
 
-options = ['-snes_type', 'srmg', '-dll_prepend', '$SRMG_DIR/$PETSC_ARCH/lib/libsrmg.so', '-MMS', '2', '-snes_srmg_levels', str(k) ,'-buffer_size', '0', '-snes_srmg_refinement_ratio', '2']
-#options = ['-snes_type', 'srmg', '-dll_prepend', '$SRMG_DIR/$PETSC_ARCH/lib/libsrmg.so', '-MMS', '2', '-snes_srmg_levels', str(k),'-buffer_size', '1', '-da_grid_x 7', '-da_grid_y 7']
+options = ['-snes_type', 'srmg', '-dll_prepend', '$SRMG_DIR/$PETSC_ARCH/lib/libsrmg.'+sharedExt, '-MMS', '2', '-snes_srmg_levels', str(k) ,'-buffer_size', '0', '-snes_srmg_refinement_ratio', '2']
+#options = ['-snes_type', 'srmg', '-dll_prepend', '$SRMG_DIR/$PETSC_ARCH/lib/libsrmg.'+sharedExt, '-MMS', '2', '-snes_srmg_levels', str(k),'-buffer_size', '1', '-da_grid_x 7', '-da_grid_y 7']
 
 cmd = '$PETSC_DIR/src/snes/examples/tutorials/ex5 '+' '.join(options)
 out = subprocess.check_output(cmd, shell=True) 
